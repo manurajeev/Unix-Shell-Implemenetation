589a3d06debdff327499b3613af70e5b912a23de
Manu Rajeev k <37786638+manurajeev@users.noreply.github.com>
2021-09-26 21:33:40 +0530

    Minor Issues fix
    Added fixes to ignore empty commands in batch mode, redirection without spaces etc.

--- tash.c
+++ tash.c
@@ -5,129 +5,206 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <ctype.h>
 
 char error_message[30] = "An error has occurred\n";
 int arg_count;
 char **path_arr;
 int path_count;
+bool redirect = false;
+char *rdfilename;
+bool toexecute = true;
 
-char **parse_cmd(char *line,char *delimiter) {
-  char **cmd_arr = malloc(sizeof(char *) * (strlen(line)+1));
-  //char *delimiter = " ";
+//Function to parse text using delimiter for space,&,>
+char **parse_cmd(char *line, char *delimiter)
+{
+  char **cmd_arr = malloc(sizeof(char *) * (strlen(line) + 1));
   char *token;
-  //get first token
   token = strtok(line, delimiter);
   int i = 0;
-  while (token != NULL) {
-   //Removing the \n that will be added at the end of the last argument
-    token[strcspn(token, "\n")] = 0;	                       
+  while (token != NULL)
+  {
+    //Removing the \n that will be added at the end of the last argument
+    token[strcspn(token, "\n")] = 0;
     cmd_arr[i] = token;
     arg_count++;
     i++;
     token = strtok(NULL, delimiter);
   }
-    cmd_arr[i] = NULL;
-    return cmd_arr;
+  cmd_arr[i] = NULL;
+  return cmd_arr;
 }
 
-int exec_cmd(char **cmd_arr) {
-  //check for redirection
-  int x;
-  bool redirect = false;
-  for(x = 0;x < arg_count; x++) {
-    if(strcmp(cmd_arr[x], ">") == 0 && x == arg_count - 2) {
-      redirect = true;
-    }
-  }
- 
+//Function to execute parsed command
+int exec_cmd(char **cmd_arr)
+{
   //Built-in Commands: exit,cd and path implementation
-  if(strcmp("exit",cmd_arr[0]) == 0){
-    if(arg_count != 1) {
+  if (strcmp("exit", cmd_arr[0]) == 0)
+  {
+    if (arg_count != 1)
+    {
       write(STDERR_FILENO, error_message, strlen(error_message));
-    }else{
+    }
+    else
+    {
       exit(0);
     }
-  } else if(strcmp("cd",cmd_arr[0]) == 0) {
-    if(cmd_arr[2] != NULL) {
+  }
+  else if (strcmp("cd", cmd_arr[0]) == 0)
+  {
+    if (cmd_arr[2] != NULL)
+    {
       write(STDERR_FILENO, error_message, strlen(error_message));
-    }else {
-     // if(cmd_arr[1] != NULL)
-      if(chdir(cmd_arr[1]) == 0) {
-        
-       } else{
-       write(STDERR_FILENO, error_message, strlen(error_message));
-     }
-   }
-
-  }else if(strcmp("path",cmd_arr[0]) == 0){
+    }
+    else
+    {
+      if (chdir(cmd_arr[1]) == 0)
+      {
+      }
+      else
+      {
+        write(STDERR_FILENO, error_message, strlen(error_message));
+      }
+    }
+  }
+  else if (strcmp("path", cmd_arr[0]) == 0)
+  {
     int i;
     path_arr[0] = NULL;
     path_count = 0;
-    for(i=1;i<arg_count;i++) {
-      path_arr[i-1] = (char*)malloc((strlen(cmd_arr[i]) + 1) * sizeof(char));
-      strcpy(path_arr[i-1], cmd_arr[i]);
-      strcat(path_arr[i-1], "/");
+    for (i = 1; i < arg_count; i++)
+    {
+      path_arr[i - 1] = (char *)malloc((strlen(cmd_arr[i]) + 1) * sizeof(char));
+      strcpy(path_arr[i - 1], cmd_arr[i]);
+      strcat(path_arr[i - 1], "/");
       path_count++;
     }
-  }else{
+  }
+  else
+  {
     bool check_exec = false;
     char temp_dir[100];
     char temp_cmd[100];
 
     int rc = fork();
-    if(rc == 0) {
+    if (rc == 0)
+    {
       //redirect the std o/p and error
-      if(redirect) {
-          char *rdfilename = cmd_arr[arg_count - 1];
-	  int file = open(rdfilename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-	  if(file<0) {
-	    write(STDERR_FILENO, error_message, strlen(error_message));
-	  }
-	  dup2(file, STDOUT_FILENO);
-	  dup2(file, STDERR_FILENO);
-	  close(file);
-	  //Remove the last two args that were used for redirection
-	  cmd_arr[arg_count-1] = NULL;
-	  cmd_arr[arg_count-2] = NULL;
-       }
+      if (redirect)
+      {
+        int file = open(rdfilename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+        if (file < 0)
+        {
+          write(STDERR_FILENO, error_message, strlen(error_message));
+        }
+        dup2(file, STDOUT_FILENO);
+        dup2(file, STDERR_FILENO);
+        close(file);
+      }
       //Checking for the executable in all the paths
       int i;
-      for(i=0;i<path_count;i++) {
+      for (i = 0; i < path_count; i++)
+      {
         strcpy(temp_dir, path_arr[i]);
-	strcat(temp_dir, cmd_arr[0]);
-	if(access(temp_dir,X_OK)==0) {
-	  strcpy(temp_cmd, temp_dir);
-	  check_exec = true;
-	  break;
-	}
+        strcat(temp_dir, cmd_arr[0]);
+        if (access(temp_dir, X_OK) == 0)
+        {
+          strcpy(temp_cmd, temp_dir);
+          check_exec = true;
+          break;
+        }
       }
-      if(check_exec) {
-        execv(temp_cmd,cmd_arr);
-      }else{
+      if (check_exec)
+      {
+        execv(temp_cmd, cmd_arr);
+      }
+      else
+      {
         write(STDERR_FILENO, error_message, strlen(error_message));
       }
-    }else if(rc>0){
-      //parent
-      //wait(NULL);
-      int status; 
-      do {
-        waitpid(rc, &status, WUNTRACED);
-     }while(!WIFEXITED(status) && !WIFSIGNALED(status));
-   }else {
+    }
+    else if (rc > 0)
+    {
+      wait(NULL);
+    }
+    else
+    {
       write(STDERR_FILENO, error_message, strlen(error_message));
     }
   }
   return 1;
 }
 
-int main(int argc, char *argv[]) {
+//Function to trim leading and trailing spaces
+void trim(char *s)
+{
+  char *p = s;
+  int l = strlen(p);
+  while (isspace(p[l - 1]))
+    p[--l] = 0;
+  while (*p && isspace(*p))
+    ++p, --l;
+  memmove(s, p, l + 1);
+}
+
+//Function to check for redirection
+char *check_rdir(char *line)
+{
+  char *linecpy = malloc(sizeof(char *) * (strlen(line) + 1));
+  strcpy(linecpy, line);
+  if (strchr(linecpy, '>') != NULL)
+  {
+    redirect = true;
+  }
+  char *cmd_wtout_rdir = NULL;
+  if (redirect)
+  {
+    char **rdir_cmd = parse_cmd(linecpy, ">");
+    int i = 0;
+    while (rdir_cmd[i] != NULL)
+    {
+      i++;
+    }
+    if (i != 2)
+    {
+      write(STDERR_FILENO, error_message, strlen(error_message));
+      redirect = false;
+      toexecute = false;
+      rdfilename = NULL;
+    }
+    cmd_wtout_rdir = rdir_cmd[0];
+    bool space = false;
+    if(rdir_cmd[1] != NULL)
+    {
+      rdfilename = rdir_cmd[1];
+      trim(rdfilename);
+      //check for spaces in middle
+      i=0;
+      while(rdfilename[i] != '\0') {
+        if(isspace(rdfilename[i])) {
+          space = true;
+        }
+        i++;
+      }
+    }
+    if(space){
+      write(STDERR_FILENO, error_message, strlen(error_message));
+      redirect = false;
+      toexecute = false;
+    }
+    trim(cmd_wtout_rdir);
+  }
+  return cmd_wtout_rdir;
+}
+
+int main(int argc, char *argv[])
+{
   char *batchFileName;
   char **cmd_arr;
   char **parallel_cmdarr;
   bool batchMode = false;
   char *buffer;
   size_t buffsize = 100;
-  //size_t characters;
   char *line = NULL;
   size_t len = 0;
   ssize_t nread;
@@ -137,61 +214,106 @@ int main(int argc, char *argv[]) {
   path_count = 1;
   arg_count = 0;
   buffer = (char *)malloc(buffsize * sizeof(char));
-  if(buffer == NULL){
+  char *rdirparsedcmd;
+  if (buffer == NULL)
+  {
     write(STDERR_FILENO, error_message, strlen(error_message));
   }
-  
-  //Handle whether to run in Batch Mode or not 
-   if(argc==2) {
-      //run shell in batch mode
-      batchMode = true;
-      batchFileName = argv[1];
-    }else if(argc != 1) {
-      //Throw error
-      write(STDERR_FILENO, error_message, strlen(error_message));
-      exit(1);
-   }
-  if(batchMode) {
+
+  //Handle whether to run in Batch Mode or not
+  if (argc == 2)
+  {
+    batchMode = true;
+    batchFileName = argv[1];
+  }
+  else if (argc != 1)
+  {
+    write(STDERR_FILENO, error_message, strlen(error_message));
+    exit(1);
+  }
+  if (batchMode)
+  {
+    //run tash in batch mode
     batchFileStream = fopen(batchFileName, "r");
-    if(batchFileStream == NULL) {
+    if (batchFileStream == NULL)
+    {
       write(STDERR_FILENO, error_message, strlen(error_message));
       exit(1);
     }
-    int p=0;
-    while((nread = getline(&line, &len, batchFileStream)) != -1) {
-     p++;
-     parallel_cmdarr = parse_cmd(line,"&");
+    int p = 0;
+    while ((nread = getline(&line, &len, batchFileStream)) != -1)
+    {
+      p++;
+      //If empty line, skip
+      trim(line);
+      if(strlen(line) == 0) {
+       continue;
+      }
+      arg_count=0;
+      parallel_cmdarr = parse_cmd(line, "&");
+      //check for empty first command
+      if(arg_count ==  0) {
+	toexecute=false;
+	write(STDERR_FILENO, error_message, strlen(error_message));
+	continue;
+      }
+    
       int i = 0;
-      while(parallel_cmdarr[i] != NULL) {
-	arg_count = 0;
-	cmd_arr = parse_cmd(parallel_cmdarr[i]," ");
-	i++;
-	if(arg_count > 0){
-	  exec_cmd(cmd_arr);
-	}
+      while (parallel_cmdarr[i] != NULL)
+      {
+        arg_count = 0;
+        //check for redirection
+        redirect = false;
+        rdirparsedcmd = check_rdir(parallel_cmdarr[i]);
+        if (rdirparsedcmd != NULL)
+        {
+          cmd_arr = parse_cmd(rdirparsedcmd, " ");
+        }
+        else
+        {
+          cmd_arr = parse_cmd(parallel_cmdarr[i], " ");
+        }
+        i++;
+        if (arg_count > 0 && toexecute)
+        {
+          exec_cmd(cmd_arr);
+        }
       }
     }
     fclose(batchFileStream);
-  }else {
-    while(1){
+  }
+  else
+  {
+    //run tash in interactive mode
+    while (1)
+    {
       printf("tash> ");
-      getline(&buffer, &buffsize, stdin);
-      
-      parallel_cmdarr = parse_cmd(buffer,"&");
+      getline(&buffer, &buffsize, stdin);    
+      parallel_cmdarr = parse_cmd(buffer, "&");
       int i = 0;
-      while(parallel_cmdarr[i] != NULL) {
+      while (parallel_cmdarr[i] != NULL)
+      {
         arg_count = 0;
-        cmd_arr = parse_cmd(parallel_cmdarr[i]," ");
-	i++;
-	if(arg_count > 0) {
-	  exec_cmd(cmd_arr);
-	}
+        //check for redirection
+        redirect = false;
+        rdirparsedcmd = check_rdir(parallel_cmdarr[i]);
+        if (rdirparsedcmd != NULL)
+        {
+          cmd_arr = parse_cmd(rdirparsedcmd, " ");
+        }
+        else
+        {
+          cmd_arr = parse_cmd(parallel_cmdarr[i], " ");
+        }
+        i++;
+        if (arg_count > 0 && toexecute)
+        {
+          exec_cmd(cmd_arr);
+        }
       }
     }
-  }  
+  }
   free(line);
   free(buffer);
   return 0;
- }
-
-
+}
